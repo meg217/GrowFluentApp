aa5e000a3f0f6e2c33c6e8009173523b409df0f0
meg217 <117707592+meg217@users.noreply.github.com>
2023-04-26 21:08:25 -0500

    flower shown upon lesson completion

--- LessonPassActivity.java
+++ LessonPassActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import edu.utsa.cs3443.growfluent.Controller.LessonPassController;
@@ -21,6 +23,24 @@ public class LessonPassActivity extends AppCompatActivity {
             int lessonNum = intent.getIntExtra("lessonNum", 0);
             int lessonComplete = intent.getIntExtra("lessonComplete", 0);
             int totalQuestions = 3;
+            ImageView Plant = findViewById(R.id.Plant);
+
+            if(lessonNum == 1) {
+                Plant.setImageResource(R.drawable.lesson1flower);
+            }
+            if(lessonNum == 2) {
+                Plant.setImageResource(R.drawable.lesson2flower);
+            }
+            if(lessonNum == 3) {
+                Plant.setImageResource(R.drawable.lesson3flower);
+            }
+
+            Plant.setLayoutParams(new ViewGroup.LayoutParams(500, 500));
+            Plant.setX(300);
+            Plant.setY(700);
+
+
+
             LessonPassController LPController = new LessonPassController(this, totalQuestions, lessonNum);
             Button Back = findViewById(R.id.Back);
             Back.setOnClickListener(LPController);
